e929528490ea6669aeb3f9d1eb34b056de44d236
coneeone <nicolaslaudinet.dev@gmail.com>
2022-10-19 23:16:32 +0200

    get the loudest frequency from fft * WIP might need to refactor code

--- get_audio_input.py
+++ get_audio_input.py
@@ -10,7 +10,8 @@ class AudioHandler():
         self.stream: pyaudio.Stream
         self.microphone_device_index = -1
         self.microphone_sample_rate = -1
-        self.CHUNKS = 8192
+        self.CHUNKS = 1024
+        self.FFT_FREQUENCY_BINS = 1024
 
     def set_input_device(self, name=""):
         host_info = self.pyAudio.get_host_api_info_by_index(0)
@@ -19,6 +20,7 @@ class AudioHandler():
         for i in range(0, host_devices_number):
             device_has_input_channels = self.pyAudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels') > 0
             device_name = self.pyAudio.get_device_info_by_host_api_device_index(0, i).get('name')
+            print(self.pyAudio.get_device_info_by_host_api_device_index(0, i))
 
             if device_has_input_channels and name in device_name:
                 self.microphone_device_index = i
@@ -41,10 +43,29 @@ class AudioHandler():
         self.stream.start_stream()
         while self.stream.is_active():
             data = numpy.fromstring(self.stream.read(self.CHUNKS, exception_on_overflow=False))
-            stft_matrix = numpy.abs(librosa.stft(data))
-            print(stft_matrix)
-            time.sleep(0.5)
+            fft_matrix = numpy.fft.fft(data, n=self.FFT_FREQUENCY_BINS)
+            loudest_frequency =  self.get_loudest_frequency_from_fft(fft=fft_matrix)
+            print(loudest_frequency)
+    
+    def get_loudest_frequency_from_fft(self, fft=[]):
+        frequencies = numpy.fft.rfftfreq(round(len(fft)) * 2 -1)
 
+        hz_frequencies = frequencies * self.microphone_sample_rate
+        absolute_fft = numpy.abs(fft)
+
+        filtered_fft = numpy.where(hz_frequencies < 20000, absolute_fft, 0)
+
+        loudest_high_index = numpy.argmax(filtered_fft)
+        loudest_low_index = loudest_high_index -1
+
+        loudest_high_range = hz_frequencies[loudest_high_index]
+
+        if loudest_low_index > -1:
+            loudest_low_range = hz_frequencies[loudest_low_index]
+        else:
+            loudest_low_range = 0
+
+        return f'{round(loudest_low_range)} Hz - {round(loudest_high_range)} Hz'
 
     def stream_end(self):
         self.stream.stop_stream()
@@ -52,6 +73,7 @@ class AudioHandler():
         self.pyAudio.terminate()  
 
 audioHandler = AudioHandler()
-audioHandler.set_input_device(name="Jabra EVOLVE LINK: USB Audio")
+# audioHandler.set_input_device(name="Jabra EVOLVE LINK: USB Audio")
+audioHandler.set_input_device(name="KT USB Audio")
 audioHandler.init_stream()
 audioHandler.stream_start()
