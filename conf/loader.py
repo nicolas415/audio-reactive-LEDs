0697f799a2918a5f26b69361a2411894d88012d9
coneeone <nicolaslaudinet.dev@gmail.com>
2023-09-06 22:41:38 +0200

    moved config.json to root

--- conf/loader.py
+++ conf/loader.py
@@ -1,5 +1,5 @@
 import json
-
-with open('conf/config.json') as config_file:
+# path of the config file is relative to where `start.py` is launched
+with open('./config.json') as config_file:
 	file_contents = config_file.read()
 	config = json.loads(file_contents)
